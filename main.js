72e7801f213edb48f8a715b13f5ebf7d2c901ecb
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2023-11-07 19:52:29 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,72 @@
+const products = [
+  {
+    name: 'Photo 1',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 2',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 3',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 4',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 5',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 6',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 7',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 8',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 9',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  },
+  {
+    name: 'Photo 10',
+    price: 2,
+    stars: 4,
+    reviews: 250,
+    seller: 'Pc Componentes'
+  }
+]
