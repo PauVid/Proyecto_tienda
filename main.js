bc229cfedb53d3d884469ae7d36251b488f67c17
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2023-11-07 20:01:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,74 +1,84 @@
-console.log('¡Funciona!')
+console.log('¡Está Activo!')
 
 const products = [
   {
-    name: 'Courduroy Country Club Hat',
+    name: 'Courduroy Country Club Hat Navy',
+    color: 'Navy',
     price: 35,
-    stars: 4,
+    stars: 5,
     reviews: 250,
-    seller: 'Pc Componentes'
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 2',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Courduroy Country Club Hat Brown',
+    color: 'Brown',
+    price: 35,
+    stars: 5,
+    reviews: 20,
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 3',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Courduroy Country Club Hat Green',
+    color: 'Green',
+    price: 35,
+    stars: 5,
+    reviews: 50,
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 4',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Pom Pom Beanie Hat White',
+    color: 'White',
+    price: 20,
+    stars: 5,
+    reviews: 2,
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 5',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Pom Pom Beanie Hat Beige',
+    color: 'Beige',
+    price: 20,
+    stars: 5,
+    reviews: 22,
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 6',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Pom Pom Beanie Hat Black',
+    color: 'Black',
+    price: 20,
+    stars: 5,
+    reviews: 130,
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 7',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Dad Hat White',
+    color: 'White',
+    price: 30,
+    stars: 5,
+    reviews: 52,
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 8',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Dad Hat Black',
+    color: 'Black',
+    price: 30,
+    stars: 5,
+    reviews: 230,
+    seller: 'FNDS'
   },
   {
-    name: 'Photo 9',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Dad Hat Cyan',
+    color: 'Cyan',
+    price: 30,
+    stars: 5,
+    reviews: 235,
+    seller: 'not FNDS'
   },
   {
-    name: 'Photo 10',
-    price: 2,
-    stars: 4,
-    reviews: 250,
-    seller: 'Pc Componentes'
+    name: 'Dad Hat Brown',
+    color: 'Brown',
+    price: 30,
+    stars: 5,
+    reviews: 40,
+    seller: 'not FNDS'
   }
 ]
