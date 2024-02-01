c2a3893d20f793a94eda7127870044c8c0b508f2
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2024-02-01 11:33:58 +0100

    proyecto2

---
+++ Proyecto2/proyecto2/main.js
@@ -0,0 +1,88 @@
+import './style.css'
+
+
+const products = [
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },  
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },  
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+  {
+    name: 'HP Essentials 255 G8 AMD',
+    price: 289,
+    starts: 4,
+    reviews: 250,
+    seller: 'PcComponentes',
+    image: 'https://img.pccomponentes.com/articles/1072/10720423/123-asus-rog-strix-g16-g614ji-n3163-intel-core-i7-13650hx-32gb-1tb-ssd-rtx-4070-16.jpg'
+  },
+
+];
+
+
