0a30e9528908f0f0b3ccb64b273f8a463e35f06f
Pau Vidal <144016810+PauVid@users.noreply.github.com>
2024-02-01 13:18:16 +0100

    b4lunch

--- Proyecto2/proyecto2/main.js
+++ Proyecto2/proyecto2/main.js
@@ -86,3 +86,46 @@ const products = [
 ];
 
 
+// Creamos los elementos e importamos los productos 
+
+const productList = () => {
+
+    for (let i = 0; i < products.length; i++){
+
+      const productName = document.createElement("h3");
+      const productPrice = document.createElement("p");
+      const productStars = document.createElement("img");
+      const productReviews = document.createElement("p");
+      const productSeller = document.createElement("p");
+      const productImage = document.createElement("img");
+      const divInfo = document.createElement("div");
+      const divProduct = document.createElement("div");
+      const divImage = document.createElement("div");
+
+
+
+      productName.textContent = products.name;
+      productPrice.textContent = products.price;
+      productStars.textContent = products.starts;
+      productReviews.textContent = products.reviews;
+      productSeller.textContent = products.seller;
+      productImage.textContent = products.image;
+      
+      
+      divInfo.append(productName);
+      divInfo.append(productPrice);
+      divInfo.append(productStars);
+      divInfo.append(productReviews);
+      divInfo.append(productSeller);
+
+      divImage.append(productImage);
+
+      divProduct.append(divImage);
+      divProduct.append(divInfo);
+
+      document.body.append(divProduct);
+
+
+
+    }
+}
